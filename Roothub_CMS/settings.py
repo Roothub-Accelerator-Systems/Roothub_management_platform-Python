a21962c75340a3f4d69e27c82c2e94ed67666ac6
HenryCey <henrycey@gmail.com>
2024-11-06 15:44:52 +0100

    this is the overall changes

--- Roothub_CMS/settings.py
+++ Roothub_CMS/settings.py
@@ -73,10 +73,23 @@ WSGI_APPLICATION = "Roothub_CMS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+#using postgresql instead of sqlite
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'roothub_cms',                    
+        'USER': 'postgres',                    
+        'PASSWORD': '85275312',             
+        'HOST': 'localhost',                       
+        'PORT': '5432',                             
     }
 }
 
